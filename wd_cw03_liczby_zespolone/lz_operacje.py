c6a5808eeba7fab34eb00a6c26fa016d21c6b758
pawel2706111 <59094564+pawel2706111@users.noreply.github.com>
2020-04-04 15:51:46 +0200

    Delete lz_operacje.py

--- wd_cw03_liczby_zespolone/lz_operacje.py
+++
@@ -1,9 +0,0 @@
-def lz_dodawanie(l1, l2):
-    l1 = complex(l1)
-    l2 = complex(l2)
-    return l1 + l2
-
-def lz_odejmowanie(l1, l2):
-    l1 = complex(l1)
-    l2 = complex(l2)
-    return l1 - l2
